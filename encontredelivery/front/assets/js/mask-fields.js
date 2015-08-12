4be67e47892cb0efe74d8db35ab4a2b67a47f1c5
Valério Bezerra <valeriobezerra.dev@gmail.com>
2015-08-12 13:42:27 -0300

    Encontre Delivery - Inicio

---
+++ encontredelivery/front/assets/js/mask-fields.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$('.mask-cpf').mask('999.999.999-99');
+	$('.mask-cep').mask('99999-999');
+	$('.mask-fone').mask('(99) 99999999?9');
+	$('.mask-date').mask('99/99/9999');
+	$('.mask-day').mask('9?9');
+	$('.mask-month').mask('9?9');
+});
+
+
+ 
